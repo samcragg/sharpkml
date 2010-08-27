0d966fd96b6428247b4cbeef62905e78e0332737
Samuel Cragg <samcragg@users.noreply.github.com>
2010-08-27 13:44:40 +0000

    First version

---
+++ SharpKml/Dom/Styles/LineStyle.cs
@@ -0,0 +1,17 @@
+﻿using SharpKml.Base;
+
+namespace SharpKml.Dom
+{
+    /// <summary>Specifies the drawing style for a line geometry.</summary>
+    /// <remarks>OGC KML 2.2 Section 12.11</remarks>
+    [KmlElement("LineStyle")]
+    public sealed class LineStyle : ColorStyle
+    {
+        /// <summary>The default value that should be used for <see cref="Width"/>.</summary>
+        public const double DefaultWidth = 1.0;
+
+        /// <summary>Gets or sets the width of the line, in pixels.</summary>
+        [KmlElement("width", 1)]
+        public double? Width { get; set; }
+    }
+}
