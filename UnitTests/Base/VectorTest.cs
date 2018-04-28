d920972fe04c0c1c6b64057d9925b3fdc7de97a8
Mikołaj Dalecki <dalecki.mikolaj@gmail.com>
2018-04-29 00:55:26 +0200

    Add test for + i * operators.

--- UnitTests/Base/VectorTest.cs
+++ UnitTests/Base/VectorTest.cs
@@ -1,5 +1,6 @@
-﻿using NUnit.Framework;
-using SharpKml.Base;
+﻿using System.Numerics;
+using NUnit.Framework;
+using Vector = SharpKml.Base.Vector;
 
 namespace UnitTests.Base
 {
@@ -42,6 +43,38 @@ namespace UnitTests.Base
             vector2.Altitude = 3.0;
             vector2.Latitude = 3.0;
             Assert.That(vector1, Is.Not.EqualTo(vector2));
+        }
+
+        [Test]
+        public void TestSimpleMultiplication()
+        {
+            Vector vector1 = new Vector();
+            vector1 *= 1;
+            Assert.AreEqual(0, vector1.Longitude);
+            Assert.AreEqual(0, vector1.Latitude);
+            Assert.AreEqual(null, vector1.Altitude);
+        }
+
+        [Test]
+        public void TestMoreMultiplication()
+        {
+            Vector vector1 = new Vector(10, 10, 10);
+            vector1 *= 10;
+            Assert.AreEqual(100, vector1.Longitude);
+            Assert.AreEqual(100, vector1.Latitude);
+            Assert.AreEqual(100, vector1.Altitude);
+        }
+
+        [Test]
+        public void TestVectorAddition()
+        {
+            Vector vector1 = new Vector(10, 10);
+            Vector vector2 = new Vector(-10, -10, -10);
+            Vector endVector = vector1 + vector2;
+
+            Assert.AreEqual(0, endVector.Longitude);
+            Assert.AreEqual(0, endVector.Latitude);
+            Assert.AreEqual(null, endVector.Altitude);
         }
     }
 }
