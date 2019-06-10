f0a1a141b664f42c0a1d7d4e8928de0accf395c0
Samuel Cragg <sam.cragg@outlook.com>
2019-06-10 21:21:09 +0100

    Update NuGet packages

--- SharpKml/GlobalSuppressions.cs
+++ SharpKml/GlobalSuppressions.cs
@@ -5,4 +5,5 @@
 
 using System.Diagnostics.CodeAnalysis;
 
+[assembly: SuppressMessage("Globalization", "CA1303:Do not pass literals as localized parameters", Justification = "We are not localizing the assembly")]
 [assembly: SuppressMessage("StyleCop.CSharp.LayoutRules", "SA1516:Elements must be separated by blank line", Justification = "Style not used")]
